ead64fac6a2e648159ffc63146fee0b06a6a4119
jakkie <jakubkielbasiewicz@gmail.com>
2022-10-25 22:36:41 +0200

    Python TrieNode

--- python/k_v_trie.py
+++ python/k_v_trie.py
@@ -1,10 +1,10 @@
 class TrieNode:
-    def __init__(self, key_char):
+    def __init__(self, key_char:str):
         self.key_char = key_char
         self._is_end = False
         self._children = {}
 
-    def hasChild(self, key_char):
+    def hasChild(self, key_char:str):
         return key_char in self._children
 
     def hasChildren(self):
@@ -15,3 +15,15 @@ class TrieNode:
 
     def getKeyChar(self):
         return self.key_char
+
+    def insertChildNode(self, key_char: str, child):
+        if not self.hasChild(key_char):
+            self._children[key_char] = child
+
+    def getChildNode(self, key_char):
+        if key_char in self._children:
+            return self._children[key_char]
+
+    def removeChildNode(self, key_char):
+        if key_char in self._children:
+            del self._children[key_char]
